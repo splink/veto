30319a7770cf05f9c99ba28dcbbd05627d56ada2
Max Kugland <maxkugland@gmail.com>
2019-03-17 00:31:43 +0100

    Improve OptionValidators and add test cases

--- src/main/scala/org/splink/veto/validators/OptionValidators.scala
+++ src/main/scala/org/splink/veto/validators/OptionValidators.scala
@@ -6,19 +6,23 @@ object OptionValidators {
 
   def optionNonEmpty[T] = Validator[Option[T]] { (opt, context) =>
     if (opt.nonEmpty) Valid(opt)
-    else Invalid(Error(context, 'optionNonEmpty, Seq(opt)))
+    else Invalid(Error(context, 'optionNonEmpty, Seq.empty))
   }
 
-  def optionalStringNonEmpty = Validator[Option[String]] { (opt, context) =>
-    opt.collect { case s if s.isEmpty =>
-      Invalid(Error(context, 'optionNonEmpty))
+  def optional[T](v: Validator[T]) = Validator[Option[T]] { (opt, context) =>
+    opt.map { t =>
+      v(t, context).map(t => Some(t))
     } getOrElse {
       Valid(opt)
     }
   }
 
   def optionalModel[T](mv: ModelValidator[T]) = Validator[Option[T]] { (opt, context) =>
-    opt.map(m => mv(m, context).map(Some(_))).getOrElse(Valid(opt))
+    opt.map { m =>
+      mv(m, context).map(Some(_))
+    }.getOrElse {
+      Valid(opt)
+    }
   }
 
 }
---
+++ src/test/scala/org/splink/veto/OptionValidatorsTest.scala
@@ -0,0 +1,33 @@
+package org.splink.veto
+
+import org.scalatest.{FlatSpec, Matchers}
+import org.splink.veto.validators.OptionValidators._
+import org.splink.veto.validators.NumericValidators._
+
+class OptionValidatorsTest extends FlatSpec with Matchers {
+
+  val emptyContext = Context("", "", "", "")
+
+  "optional" should "validate the value wrapped in an Option and succeed if it is valid" in {
+    val validator = optional[Int](isPositive[Int])
+    validator(Some(1), emptyContext) should equal(Valid(Some(1)))
+  }
+
+  it should "validate the value wrapped in an Option and fail if it is invalid" in {
+    val validator = optional[Int](isPositive[Int])
+    validator(Some(-1), emptyContext) should equal(
+      Invalid(
+        Error(emptyContext, 'isPositive, Seq(-1))
+      )
+    )
+  }
+
+  it should "fail if combined with a optionNonEmpty" in {
+    val validator = optional[Int](isPositive[Int]) and optionNonEmpty
+    validator(None, emptyContext) should equal(
+      Invalid(
+        Error(emptyContext, 'optionNonEmpty, Seq())
+      )
+    )
+  }
+}
