2d6a20033f9708d25bd09182044e2a6c672e72e5
Max Kugland <maxkugland@gmail.com>
2019-03-23 23:27:43 +0100

    extend IntegrationTest to also check ListValidator and recursive validation

--- src/test/scala/org/splink/veto/IntegrationTest.scala
+++ src/test/scala/org/splink/veto/IntegrationTest.scala
@@ -6,23 +6,30 @@ import org.scalatest.{FlatSpec, Matchers}
 import org.splink.veto.validators.NumericValidators._
 import org.splink.veto.validators.StringValidators._
 import org.splink.veto.validators.OptionValidators._
+import org.splink.veto.validators.ListValidator
+
 
 class IntegrationTest extends FlatSpec with Matchers {
 
-  case class Item(id: Option[Id], size: Size, visibility: Visibility)
+  case class Item(id: Option[Id], size: Size, visibility: Visibility, more: List[Item])
+
   case class Size(width: Int, height: Int)
+
   case class Id(value: String)
 
   sealed trait Visibility
+
   case object Invisible extends Visibility
+
   case object Visible extends Visibility
 
   object ItemValidator extends ModelValidator[Item] {
     override def apply(item: Item)(implicit parent: Option[Context]) =
       Check(item)
-        .field(_.id, "item.id")(optional(idValidator))
-        .field(_.size, "item.size")(SizeValidator)
-        .field(item => (item.size, item.visibility), "item.visibility")(VisibilityValidator.apply)
+        .field(_.id, "id")(optional(IdValidator))
+        .field(_.size, "size")(SizeValidator)
+        .field(item => (item.size, item.visibility), "visibility")(VisibilityValidator.apply)
+        .field(_.more, "more")(ListValidator(ItemValidator))
         .validate
   }
 
@@ -41,10 +48,12 @@ class IntegrationTest extends FlatSpec with Matchers {
     }
   }
 
-  def idValidator = Validator[Id] { (id, context) =>
-    Check(id)
-      .field(_.value, "value")(stringNonEmpty and stringIsUUID)(Some(context)) //TODO easy to forget
-      .validate
+  object IdValidator extends ModelValidator[Id] {
+    override def apply(id: Id)(implicit context: Option[Context] = None) = {
+      Check(id)
+        .field(_.value, "value")(stringNonEmpty and stringIsUUID)
+        .validate
+    }
   }
 
   object SizeValidator extends ModelValidator[Size] {
@@ -57,7 +66,8 @@ class IntegrationTest extends FlatSpec with Matchers {
   }
 
   "ModelValidator[Item]" should "declare a correct model Valid" in {
-    val item = Item(Some(Id(UUID.randomUUID().toString)), Size(4, 4), Visible)
+    val more = Item(Some(Id(UUID.randomUUID().toString)), Size(4, 4), Visible, Nil) :: Nil
+    val item = Item(Some(Id(UUID.randomUUID().toString)), Size(4, 4), Visible, more)
 
     ItemValidator(item) should equal(
       Valid(item)
@@ -65,31 +75,44 @@ class IntegrationTest extends FlatSpec with Matchers {
   }
 
   it should "output a validation error if a model is Invalid" in {
-    val item = Item(Some(Id("123")), Size(4, 4), Visible)
+    val item = Item(Some(Id("123")), Size(4, 4), Visible, Nil)
 
     ItemValidator(item) should equal(
       Invalid(
-        Error(Context(item.id.get, "item.id.value", "123"), 'stringIsUUID, Seq("123"))
+        Error(Context(item.id.get, "id.value", "123"), 'stringIsUUID, Seq("123"))
       )
     )
   }
 
   it should "output all validation errors if a model has multiple invalid fields" in {
-    val item = Item(Some(Id("")), Size(4, 4), Visible)
+    val item = Item(Some(Id("")), Size(4, 4), Visible, Nil)
 
     ItemValidator(item) should equal(
       Invalid(
-        Error(Context(item.id.get, "item.id.value", ""), 'stringNonEmpty, Seq()),
-        Error(Context(item.id.get, "item.id.value", ""), 'stringIsUUID, Seq(""))
+        Error(Context(item.id.get, "id.value", ""), 'stringNonEmpty, Seq()),
+        Error(Context(item.id.get, "id.value", ""), 'stringIsUUID, Seq(""))
       ))
   }
 
   it should "output all validation errors if a field is invalid, whose value depends on other fields" in {
-    val item = Item(Some(Id(UUID.randomUUID().toString)), Size(0, 0), Visible)
+    val item = Item(Some(Id(UUID.randomUUID().toString)), Size(0, 0), Visible, Nil)
 
     ItemValidator(item) should equal(
       Invalid(
-        Error(Context(item, "item.visibility", (Size(0, 0), Visible)), 'visibilitySizeZero, Seq(Visible, Size(0, 0)))
+        Error(Context(item, "visibility", (Size(0, 0), Visible)), 'visibilitySizeZero, Seq(Visible, Size(0, 0)))
+      ))
+  }
+
+  it should "output all validation errors if a nested Item is invalid" in {
+    val more = Item(Some(Id(UUID.randomUUID().toString)), Size(1, 1), Visible, Nil) ::
+      Item(Some(Id(UUID.randomUUID().toString)), Size(0, 0), Visible, Nil) :: Nil
+    val item = Item(Some(Id(UUID.randomUUID().toString)), Size(2, 3), Visible, more)
+
+    ItemValidator(item) should equal(
+      Invalid(
+        Error(
+          Context(more.tail.head, "more[1].visibility", (Size(0, 0), Visible)),
+          'visibilitySizeZero, Seq(Visible, Size(0, 0)))
       ))
   }
 
